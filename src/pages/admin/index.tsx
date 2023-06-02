66f10fc463f2317c8b6f37619112adb8801e0ab6
Negar Sarani <negar.sarani2000@gmail.com>
2023-06-02 11:49:32 +0330

    add folder admin and index

---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return <div className="bg-violet-500">wqdoewjvfindv</div>;
+}
+
+AdminPage.getLayout = function PageLayout(page: any) {
+  return <> {page}</>;
+};
