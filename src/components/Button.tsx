227a51449f222f1512c16ec068ce6d4de132f0a6
Negar Sarani <negar.sarani2000@gmail.com>
2023-06-02 11:26:42 +0330

    add Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+type Buttontype = {
+  className: string;
+  children: ReactNode;
+  type: 'button' | 'submit' | 'reset' | undefined;
+  onClick?: (e:any) => void;
+};
+const Button = ({ className, children, type, onClick }: Buttontype) => {
+  return (
+    <button onClick={onClick} type={type} className={className}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
