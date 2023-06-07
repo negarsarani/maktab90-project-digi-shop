280e1acdf378a64d00e9a871f8d1c868d20c189b
Negar Sarani <negar.sarani2000@gmail.com>
2023-06-07 11:21:19 +0330

    create popover Menu

--- src/page/user/navbar/category/index.tsx
+++ src/page/user/navbar/category/index.tsx
@@ -1,9 +1,15 @@
+import React, { useState, useRef } from 'react';
+import PopoverMenu from './PopoverMenu';
 interface Props {}
 const CategoryNav = (props: Props) => {
+
   return (
-    <div className=" lg:flex  hidden px-6 xl:px-8  ">
-      <div className=" border-t mt-2 w-full py-2">
-        <span>صفحه اصلی</span>
+    <div className="lg:flex  hidden px-6 xl:px-8">
+      <div className="flex mt-2 border-t  w-full  gap-3">
+        <div className=" border-l px-2 border-black py-2">
+          <span>صفحه اصلی</span>
+        </div>
+        <PopoverMenu/>
       </div>
     </div>
   );
