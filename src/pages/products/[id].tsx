685780155c141a35662d4e862f8a8302cb3608ae
Negar Sarani <negar.sarani2000@gmail.com>
2023-06-20 10:33:56 +0330

    add url to srr [id]

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,10 +1,32 @@
+import getData from '@/api/getData';
 
-const SingleProduct = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const SingleProduct = ({}) => {
+  return <div></div>;
 };
 
 export default SingleProduct;
+
+export const getServerSideProps = async ({ params }) => {
+
+  const Dataproduct = await getData(`/products?${params.id}`).then(
+    (res: any) => res?.data?.categories
+  );
+
+  try {
+    return {
+      props: {
+        data: Dataproduct,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        data: ['lll'],
+      },
+    };
+  }
+};
+
+SingleProduct.getLayout = function PageLayout(page: any) {
+  return <>{page}</>;
+};
