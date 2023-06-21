696bec319e396d77d1c598228fe7dd9fad8537ac
Negar Sarani <negar.sarani2000@gmail.com>
2023-06-21 07:42:30 +0330

    fix bugs of buttons paginate

--- src/page/user/categories/shared/paginate.tsx
+++ src/page/user/categories/shared/paginate.tsx
@@ -19,18 +19,22 @@ const Paginate = ({ total_pages }: props) => {
       <div className=" grid grid-cols-3 w-full ">
         <div className="flex justify-start">
           {total_pages !== 1 ? (
-            router.query.page ? 
-            <a
-              className="relative inline-flex items-center rounded-md border cursor-pointer border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-              id="Prev"
-              onClick={() => {
-                let page: number = router.query.page && +router.query.page - 1;
-                page = page < 1 ? 1 : page;
-                HandlePagination(page);
-              }}
-            >
-              صفحه قبلی
-            </a> : <span></span>
+            router.query.page && +router.query.page !== 1 ? (
+              <a
+                className="relative inline-flex items-center rounded-md border cursor-pointer border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                id="Prev"
+                onClick={() => {
+                  let page: number =
+                    router.query.page && +router.query.page - 1;
+                  page = page < 1 ? 1 : page;
+                  HandlePagination(page);
+                }}
+              >
+                صفحه قبلی
+              </a>
+            ) : (
+              <span></span>
+            )
           ) : (
             <span></span>
           )}
@@ -41,19 +45,23 @@ const Paginate = ({ total_pages }: props) => {
         </span>
         <div className="flex justify-end ">
           {total_pages !== 1 ? (
-            <a
-              className="relative  inline-flex items-center cursor-pointer rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-              id="Next"
-              onClick={() => {
-                let page: number = router.query.page
-                  ? +router.query.page + 1
-                  : 2;
-                page = +page > total_pages ? total_pages : page;
-                HandlePagination(page);
-              }}
-            >
-              صفحه بعدی
-            </a> 
+            +router.query.page !== total_pages ? (
+              <a
+                className="relative  inline-flex items-center cursor-pointer rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                id="Next"
+                onClick={() => {
+                  let page: number = router.query.page
+                    ? +router.query.page + 1
+                    : 2;
+                  page = +page > total_pages ? total_pages : page;
+                  HandlePagination(page);
+                }}
+              >
+                صفحه بعدی
+              </a>
+            ) : (
+              <span> </span>
+            )
           ) : (
             <span> </span>
           )}
