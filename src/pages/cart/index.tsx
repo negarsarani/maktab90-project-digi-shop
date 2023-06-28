c4676f47420b01edbf3ab83b48ff38d5d84b983b
Negar Sarani <negar.sarani2000@gmail.com>
2023-06-28 13:56:22 +0330

    .

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,15 +1,25 @@
 import getData from '@/api/getData';
 import useRedux from '@/hooks/useRedux';
 import { MainCart } from '@/page/user';
+import Image from 'next/image';
 
 const Main = () => {
   const [value, dispatch] = useRedux((state) => state.userState);
   if (value.cart.length <= 0) {
-    return <div>سبد خرید شما خالی می باشد</div>;
+    return (
+      <div className="flex py-20 items-center justify-center] flex-col gap-2">
+        <Image
+          src={'/icons/user/emptyShop.svg'}
+          alt="سبد خرید"
+          width={300}
+          height={200}
+        />{' '}
+        <div>سبد خرید شما خالی می باشد</div>
+      </div>
+    );
   }
   if (!value.cart) {
     return <div>در حال پردازش داده های سبد خرید</div>;
-
   }
   return (
     <div className="xl:container xl:mx-auto px-4 ">
